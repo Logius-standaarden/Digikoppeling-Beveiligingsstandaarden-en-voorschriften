25343f584446913f97ce2ff36082386c354928ad
Alexander Green <alexander.green@logius.nl>
2025-12-04 16:16:12 +0100

    Geen hard-coded PDF-naam meer

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "Digikoppeling-Beveiligingsstandaarden-en-voorschriften.pdf"
-      } ],
   authors: [ { 
         "name" : "Pieter Hering",
         "url" : "https://logius.nl/standaarden"
